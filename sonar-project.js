f76b4f4fdc048ab7da66a94c7520dd42275ad9b1
sai natraj thatavarthy <105161653+saithatavarthy@users.noreply.github.com>
2022-06-21 19:01:45 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.7.70.252:9000/',
+  serverUrl: 'http://43.204.144.198:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
